b694e1add9f54cd9ac1d1aade08e0a73d449d2d4
ces-hau-bui <hau.bui@codeenginestudio.com>
2021-03-15 17:12:08 +0700

    Add Theme park ride job

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,18 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/buitahau/build-springboot-by-jenkin-demo.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
